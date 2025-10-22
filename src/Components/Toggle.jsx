20a561dece6b0d376329121e396e9e32d6ff0b16
DavidSalinas04 <A00838212@tec.mx>
2025-10-22 19:11:45 +0200

    fixed bug

--- src/Components/Toggle.jsx
+++ src/Components/Toggle.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/Components/ui/button";
 
 export function ThemeToggle() {
     const [theme, setTheme] = useState("dark");
